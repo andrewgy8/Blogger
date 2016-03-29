3b275b33f541ce546561f2d5e095efd38c5a9191
andrewgy8 <andrewgy8@gmail.com>
2016-03-29 20:23:04 +0800

    The addition of flashes and the basic workflow

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
+
 	def index
 		@articles = Article.all
 		
@@ -7,4 +9,38 @@ class ArticlesController < ApplicationController
 		@article = Article.find(params[:id])
 		
 	end
+	def new
+		@article = Article.new
+	end
+	def create
+		@article = Article.new(article_params)
+		@article.save
+
+		flash.notice = "Article '#{@article.title}' Created Successfully!"
+
+		redirect_to article_path(@article)
+	end
+	def edit
+		@article = Article.find(params[:id])
+				
+	end
+	def update
+		@article = Article.find(params[:id])
+		@article.update(article_params)
+
+		flash.notice = "Article '#{@article.title}' Updated Successfully!"
+
+		redirect_to article_path(@article)
+	end
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		flash.notice = "Article '#{@article.title}' Destroyed Successfully"
+
+		redirect_to articles_path
+
+		
+	end
+	
 end
